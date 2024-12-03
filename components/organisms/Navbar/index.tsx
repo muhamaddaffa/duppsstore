3abe56089ddc3f1f006ec947611500aa7abd7be8
Muhamad Daffa <daffafa09@gmail.com>
2024-12-03 16:44:02 +0700

    chore: add file bootstrap

--- components/organisms/Navbar/index.tsx
+++ components/organisms/Navbar/index.tsx
@@ -5,12 +5,6 @@ import Auth from "./Auth";
 import ToggleMenu from "./ToggleMenu";
 
 export default function Navbar() {
-  useEffect(() => {
-    // Ensure Bootstrap JS is loaded only on the client
-    if (typeof window !== "undefined" && typeof document !== "undefined") {
-      require("bootstrap/dist/js/bootstrap.bundle.min");
-    }
-  }, []);
   return (
     <section>
       <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
